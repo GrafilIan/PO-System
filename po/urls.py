a4cbeb6e832081e889d8121192d1430a1997352a
GrafilIan <iancarlgrafil@sorsu.edu.ph>
2024-08-06 22:57:18 +0800

    Setting up login template

--- po/urls.py
+++ po/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from django.conf import settings
 from . import views
+from .views import login_view, dashboard_view, logout_view
 
-app_name = 'po'
 urlpatterns = [
-    path('sample/', views.index, name='index'),
+    path('supplier/', views.index, name='index'),
+    path('', login_view, name='login'),
+    path('dashboard/', dashboard_view, name='dashboard'),
+    path('logout/', logout_view, name='logout'),
 ]
--- po/views.py
+++ po/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request, 'supplier/sample.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid username or password')
+            return render(request, 'authentication/login.html', {'error': 'Invalid credentials'})
+    return render(request, 'authentication/login.html')
+
+@login_required
+def dashboard_view(request):
+    if request.user.groups.filter(name='Accountant').exists():
+        return render(request, 'dashboards/accountant_dashboard.html')
+    elif request.user.groups.filter(name='Front Desk').exists():
+        return render(request, 'dashboards/front_desk_dashboard.html')
+    elif request.user.groups.filter(name='Inventory Manager').exists():
+        return render(request, 'dashboards/inventory_clerk_dashboard.html')
+    else:
+        return HttpResponse('No role assigned')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
