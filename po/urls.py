42498edf9fe1bc51c70b35faaa918341589ac778
GrafilIan <iancarlgrafil@sorsu.edu.ph>
2024-08-12 09:53:27 +0800

    Excel Export

--- po/urls.py
+++ po/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from django.conf import settings
 from . import views
-from .views import login_view, dashboard_view, logout_view, purchase_order_list
+from .views import login_view, dashboard_view, logout_view, purchase_order_list, export_orders_to_excel
 
 urlpatterns = [
-    path('supplier/', views.index, name='index'),
     path('', login_view, name='login'),
     path('dashboard/', dashboard_view, name='dashboard'),
     path('logout/', logout_view, name='logout'),
     path('purchase-order/create/', views.purchase_order_create, name='purchase_order_create'),
     path('purchase-order/success/', views.purchase_order_success, name='purchase_order_success'),
-    path('purchase-orders/', views.purchase_order_list, name='dashboard'),
+    path('purchase-orders/', views.purchase_order_list, name='purchase_order_list'),
+    path('purchase_order/edit/<int:id>/', views.purchase_order_edit, name='purchase_order_edit'),
+    path('export-orders/', export_orders_to_excel, name='export_orders_to_excel'),
+
 ]
--- po/views.py
+++ po/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
 from django.contrib import messages
 from .forms import PurchaseOrderForm
 from .models import PurchaseOrder
-
+from datetime import datetime
+import openpyxl
+from io import BytesIO
 
 # Create your views here.
-def index(request):
-    return render(request, 'supplier/sample.html')
 
 def login_view(request):
     if request.method == 'POST':
@@ -29,8 +31,7 @@ def dashboard_view(request):
     if request.user.groups.filter(name='Accountant').exists():
         return render(request, 'dashboards/accountant_dashboard.html')
     elif request.user.groups.filter(name='Front Desk').exists():
-        orders = PurchaseOrder.objects.all()
-        return render(request, 'dashboards/front_desk_dashboard.html', {'orders': orders})
+        return purchase_order_list(request)
     elif request.user.groups.filter(name='Inventory Manager').exists():
         return render(request, 'dashboards/inventory_clerk_dashboard.html')
     elif request.user.is_superuser:
@@ -38,9 +39,11 @@ def dashboard_view(request):
     else:
         return HttpResponse('No role assigned')
 
+
 def logout_view(request):
     logout(request)
     return redirect('login')
+
 
 # For Creating or Adding Records
 
@@ -58,9 +61,222 @@ def purchase_order_create(request):
     return render(request, 'records/purchase_order_form.html', {'form': form})
 
 
+
+
 def purchase_order_success(request):
     return render(request, 'records/purchase_order_success.html')
 
+
+# For Viewing Records
 def purchase_order_list(request):
-    orders = PurchaseOrder.objects.all()
+    query = request.GET.get('q')  # 'q' is the name of the search input field
+    date_query = request.GET.get('date')  # 'date' is the name of the date input field
+
+    # Start with all orders
+    orders_list = PurchaseOrder.objects.all()
+
+    # If there's a search query, filter the orders accordingly
+    if query:
+        orders_list = orders_list.filter(
+            Q(date__icontains=query) |
+            Q(po_number__icontains=query) |
+            Q(purchaser__icontains=query) |
+            Q(brand__icontains=query) |
+            Q(item_code__icontains=query) |
+            Q(particulars__icontains=query) |
+            Q(quantity__icontains=query) |
+            Q(unit__icontains=query) |
+            Q(price__icontains=query) |
+            Q(total_amount__icontains=query) |
+            Q(site_delivered__icontains=query) |
+            Q(fbbd_ref_number__icontains=query) |
+            Q(remarks__icontains=query) |
+            Q(supplier__icontains=query) |
+            Q(delivery_ref__icontains=query) |
+            Q(delivery_no__icontains=query) |
+            Q(invoice_type__icontains=query) |
+            Q(invoice_no__icontains=query) |
+            Q(payment_req_ref__icontains=query) |
+            Q(payment_details__icontains=query) |
+            Q(remarks2__icontains=query)
+        )
+
+    # If there's a date query, filter by date
+    if date_query:
+        try:
+            # Try parsing full date (e.g., "Aug 11, 2024")
+            date_obj = datetime.strptime(date_query, '%b %d, %Y').date()
+            orders_list = orders_list.filter(date=date_obj)
+        except ValueError:
+            try:
+                # Try parsing month and year only (e.g., "Aug 2024")
+                date_obj = datetime.strptime(date_query, '%b %Y')
+                orders_list = orders_list.filter(date__year=date_obj.year, date__month=date_obj.month)
+            except ValueError:
+                try:
+                    # Try parsing full month name and year (e.g., "August 2024")
+                    date_obj = datetime.strptime(date_query, '%B %Y')
+                    orders_list = orders_list.filter(date__year=date_obj.year, date__month=date_obj.month)
+                except ValueError:
+                    try:
+                        # Try parsing month only (e.g., "08" for August or "11" for November)
+                        month_number = int(date_query)
+                        orders_list = orders_list.filter(date__month=month_number)
+                    except ValueError:
+                        try:
+                            # Try parsing full month name only (e.g., "August")
+                            date_obj = datetime.strptime(date_query, '%B')
+                            orders_list = orders_list.filter(date__month=date_obj.month)
+                        except ValueError:
+                            pass  # Handle invalid date input gracefully
+
+
+
+    paginator = Paginator(orders_list, 20)  # Paginate after every 20 entries
+    page_number = request.GET.get('page')
+    orders = paginator.get_page(page_number)
+
     return render(request, 'dashboards/front_desk_dashboard.html', {'orders': orders})
+
+
+def purchase_order_edit(request, id):
+    order = get_object_or_404(PurchaseOrder, id=id)
+
+    if request.method == 'POST':
+        form = PurchaseOrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+                return JsonResponse({'status': 'success'})
+            return redirect('dashboard')
+        else:
+            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+                return JsonResponse({'status': 'error'})
+    else:
+        form = PurchaseOrderForm(instance=order)
+
+    return render(request, 'records/purchase_order_edit.html', {'form': form, 'order': order})
+
+
+# For Exporting Records
+
+def export_orders_to_excel(request):
+    query = request.GET.get('q')  # Search query parameter
+    date_query = request.GET.get('date')  # Date query parameter
+
+    # Start with all orders
+    orders_list = PurchaseOrder.objects.all()
+
+    # Apply search filter
+    if query:
+        orders_list = orders_list.filter(
+            Q(date__icontains=query) |
+            Q(po_number__icontains=query) |
+            Q(purchaser__icontains=query) |
+            Q(brand__icontains=query) |
+            Q(item_code__icontains=query) |
+            Q(particulars__icontains=query) |
+            Q(quantity__icontains=query) |
+            Q(unit__icontains=query) |
+            Q(price__icontains=query) |
+            Q(total_amount__icontains=query) |
+            Q(site_delivered__icontains=query) |
+            Q(fbbd_ref_number__icontains=query) |
+            Q(remarks__icontains=query) |
+            Q(supplier__icontains=query) |
+            Q(delivery_ref__icontains=query) |
+            Q(delivery_no__icontains=query) |
+            Q(invoice_type__icontains=query) |
+            Q(invoice_no__icontains=query) |
+            Q(payment_req_ref__icontains=query) |
+            Q(payment_details__icontains=query) |
+            Q(remarks2__icontains=query)
+        )
+
+    # Apply date filter
+    if date_query:
+        try:
+            date_obj = datetime.strptime(date_query, '%b %d, %Y').date()
+            orders_list = orders_list.filter(date=date_obj)
+        except ValueError:
+            try:
+                date_obj = datetime.strptime(date_query, '%b %Y')
+                orders_list = orders_list.filter(date__year=date_obj.year, date__month=date_obj.month)
+            except ValueError:
+                try:
+                    date_obj = datetime.strptime(date_query, '%B %Y')
+                    orders_list = orders_list.filter(date__year=date_obj.year, date__month=date_obj.month)
+                except ValueError:
+                    try:
+                        month_number = int(date_query)
+                        orders_list = orders_list.filter(date__month=month_number)
+                    except ValueError:
+                        try:
+                            date_obj = datetime.strptime(date_query, '%B')
+                            orders_list = orders_list.filter(date__month=date_obj.month)
+                        except ValueError:
+                            pass
+
+    # Create a workbook and a sheet
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    sheet.title = 'Purchase Orders'
+
+    # Define the headers
+    headers = [
+        'Date', 'PO Number', 'Purchaser', 'Brand', 'Item Code', 'Particulars',
+        'Quantity', 'Unit', 'Price', 'Total Amount',
+        'Site Delivered', 'FBBD Ref#', 'Remarks', 'Supplier', 'Delivery Ref#',
+        'Delivery No.', 'Invoice Type', 'Invoice No.', 'Payment Req Ref#',
+        'Payment Details', 'Remarks2'
+    ]
+    sheet.append(headers)
+
+    currency_format = '#,##0.00'
+
+    # Populate the sheet with data
+    for order in orders_list:
+        sheet.append([
+            order.date.strftime('%Y-%m-%d'), order.po_number, order.purchaser, order.brand, order.item_code,
+            order.particulars, order.quantity, order.unit, order.price, order.total_amount, order.site_delivered,
+            order.fbbd_ref_number, order.remarks, order.supplier, order.delivery_ref, order.delivery_no,
+            order.invoice_type, order.invoice_no, order.payment_req_ref, order.payment_details, order.remarks2
+        ])
+
+    for cell in sheet['I']:  # Assuming price is in column I (index 9)
+        if cell.row > 1:  # Skip header row
+            cell.number_format = currency_format
+
+    for cell in sheet['J']:  # Assuming total_amount is in column J (index 10)
+        if cell.row > 1:  # Skip header row
+            cell.number_format = currency_format
+
+    for column in sheet.columns:
+        max_length = 0
+        column_letter = column[0].column_letter  # Get the column name (e.g., 'A', 'B', etc.)
+        for cell in column:
+            try:
+                if len(str(cell.value)) > max_length:
+                    max_length = len(cell.value)
+            except:
+                pass
+        adjusted_width = (max_length + 2)  # Add extra space for better visibility
+        sheet.column_dimensions[column_letter].width = adjusted_width
+
+    # Create an in-memory buffer
+    buffer = BytesIO()
+    workbook.save(buffer)
+    buffer.seek(0)
+
+    # Set the response to return the Excel file
+    response = HttpResponse(
+        buffer,
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
+    response['Content-Disposition'] = 'attachment; filename=PurchaseOrders.xlsx'
+    return response
+
+
+
+
+
